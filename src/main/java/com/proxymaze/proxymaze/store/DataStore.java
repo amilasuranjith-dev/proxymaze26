f1aa00a807322c7da99dddabe53c37073029df06
amilasuranjith-dev <amilasuranjith.dev@gmail.com>
2026-05-09 21:00:20 +0530

    refactor: remove unused import for ConcurrentHashMap in DataStore

--- src/main/java/com/proxymaze/proxymaze/store/DataStore.java
+++ src/main/java/com/proxymaze/proxymaze/store/DataStore.java
@@ -4,7 +4,6 @@ import com.proxymaze.proxymaze.model.*;
 import org.springframework.stereotype.Component;
 
 import java.util.*;
-import java.util.concurrent.ConcurrentHashMap;
 import java.util.concurrent.CopyOnWriteArrayList;
 import java.util.concurrent.atomic.AtomicLong;
 import java.util.stream.Collectors;
