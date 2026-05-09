bd7ae6be96aa6c7bfb170f9c713b60ec5bc81554
amilasuranjith-dev <amilasuranjith.dev@gmail.com>
2026-05-09 15:41:12 +0530

    chore: initialize project

---
+++ src/test/java/com/proxymaze/proxymaze/ProxymazeApplicationTests.java
@@ -0,0 +1,13 @@
+package com.proxymaze.proxymaze;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ProxymazeApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
